e20204fe4f40cad0ac78070cc2aaf41c507c9198
dubo_ <277627117@qq.com>
2018-09-12 16:16:14 +0800

    test

---
+++ app/src/main/java/com/example/dubo/androidloaction/LocationUtils.java
@@ -0,0 +1,123 @@
+package com.example.dubo.androidloaction;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class LocationUtils {
+    private LocationManager locationManager;
+    private static final int REQUEST_CONTACTS = 1;
+    private static final String[] PERMISSIONS_CONTACT = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    } ;
+    private Location location;
+    static LocationUtils locationUtils;
+    MainActivity activity;
+
+    public static LocationUtils getInstance() {
+        if (locationUtils == null) {
+            locationUtils = new LocationUtils();
+        }
+        return locationUtils;
+    }
+
+    public Location getLocations(final MainActivity activity) {
+        this.activity = activity;
+        try {
+            locationManager = (LocationManager)activity.getSystemService(Context.LOCATION_SERVICE);
+            List<String> providers = locationManager.getProviders(true);//// true 代表从打开的设备中查找
+
+            String locationProvider = null;
+            if (providers.contains(LocationManager.GPS_PROVIDER)) {
+                //如果是GPS
+                locationProvider = LocationManager.GPS_PROVIDER;
+            } else if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
+                //如果是Network
+                locationProvider = LocationManager.NETWORK_PROVIDER;
+            } else {
+                Intent i = new Intent();
+                i.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                activity.startActivity(i);
+            }
+            if (Build.VERSION.SDK_INT>=23){
+                if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        || ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestSetPermissions();
+                }else {
+                    location = locationManager.getLastKnownLocation(locationProvider);
+                }
+            }else{
+                location = locationManager.getLastKnownLocation(locationProvider);
+            }
+            locationManager.requestLocationUpdates(locationProvider, 10000l, 0f, locationListener);
+            }catch (Exception e){}
+            return location;
+    }
+
+    private void requestSetPermissions() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_COARSE_LOCATION) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)  ){
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_CONTACT, REQUEST_CONTACTS);
+        }
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+
+        // Provider的状态在可用、暂时不可用和无服务三个状态直接切换时触发此函数
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        // Provider被enable时触发此函数，比如GPS被打开
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        // Provider被disable时触发此函数，比如GPS被关闭
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        //当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发
+        @Override
+        public void onLocationChanged(Location location) {
+            //DubLogUtils.i("=================location.."+location);
+        }
+    };
+
+    /**
+     * @param latitude 经度
+     * @param longitude 纬度
+     * @return 详细位置信息 GeoCoder是基于后台backend的服务，因此这个方法不是对每台设备都适用。
+     */
+    public String convertAddress(Context context, double latitude, double longitude) {
+        Geocoder mGeocoder = new Geocoder(context, Locale.getDefault());
+        StringBuilder mStringBuilder = new StringBuilder();
+        try {
+            List<Address> mAddresses = mGeocoder.getFromLocation(latitude, longitude, 1);
+            if (!mAddresses.isEmpty()) {
+                Address address = mAddresses.get(0);
+                mStringBuilder.append(address.getCountryName()).append(", ").append(address.getAdminArea()).append(", ").append(address.getLocality());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return mStringBuilder.toString();
+    }
+}
